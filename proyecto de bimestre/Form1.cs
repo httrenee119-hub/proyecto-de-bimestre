3f7b2102208869447867845396e9ce3125fffb79
4º PC <4 PC@DESKTOP-4847ESQ>
2026-03-17 11:23:09 -0600

    suma

--- proyecto de bimestre/Form1.cs
+++ proyecto de bimestre/Form1.cs
@@ -31,7 +31,31 @@ namespace proyecto_de_bimestre
             if (textBox2.Text == "")
             {
                 MessageBox.Show(" error, Ingrese un numero en el segundo campo ");
+               
             }
+            //checar si los datos ingresados sean numero 
+            double num1, num2;
+           
+            if (!double.TryParse(textBox1.Text, out num1))
+            {
+                MessageBox.Show(" error, Ingrese un numero valido en el primer campo ");
+                return;
+            }
+            if (!double.TryParse(textBox2.Text, out num2))
+            {
+                MessageBox.Show(" error, Ingrese un numero valido en el segundo campo ");
+                return;
+            }
+
+            double resultado = num1 + num2;
+            label3.Text = resultado.ToString();
+
+        }
+
+        private void label3_Click(object sender, EventArgs e)
+        {
+            
+
         }
     }
 }
