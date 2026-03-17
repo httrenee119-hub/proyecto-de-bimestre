6d5c655dc5c139dd9442d9efde4522282e02575b
4º PC <4 PC@DESKTOP-4847ESQ>
2026-03-17 08:50:59 -0600

    Agregar archivos de proyecto.

---
+++ proyecto de bimestre/Form1.cs
@@ -0,0 +1,37 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel;
+using System.Data;
+using System.Drawing;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows.Forms;
+
+namespace proyecto_de_bimestre
+{
+    public partial class Form1 : Form
+    {
+        public Form1()
+        {
+            InitializeComponent();
+        }
+
+        private void label1_Click(object sender, EventArgs e)
+        {
+
+        }
+
+        private void button1_Click(object sender, EventArgs e)
+        {
+            if (textBox1.Text == "")
+            {
+                MessageBox.Show(" error, Ingrese un numero en el primer campo ");
+            }
+            if (textBox2.Text == "")
+            {
+                MessageBox.Show(" error, Ingrese un numero en el segundo campo ");
+            }
+        }
+    }
+}
